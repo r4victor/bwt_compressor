08b21fa4f92d88b6f27495c8bc930d2ab1e98102
Victor Skvortsov <vds003@gmail.com>
2021-05-28 15:57:07 +0500

    Optimize dc decoding by using a segment tree

--- bwt_compressor/dc.py
+++ bwt_compressor/dc.py
@@ -47,59 +47,15 @@ def _remove_redundant_ones(char_distances, char_distances_reduced):
     return [d for d in res if d is not None]
 
 
-# def dc_decode(dc):
-#     text_length = dc[0]
-#     text = list(_get_alphabet()) + [None] * text_length
-#     known_chars_st = SegmentTree([1] * ALPHABET_SIZE + [0] * text_length)
-
-#     next_empty_index = ALPHABET_SIZE
-#     i = 0
-#     for d in dc[1:]:
-#         # restore consecutive chars
-#         while i < len(text) - 1 and text[i+1] is None:
-#             text[i+1] = text[i]
-#             known_chars_st.update(i+1, 1)
-#             i += 1
-        
-#         if d == 0:
-#             i += 1
-#             continue
-        
-#         # find the dth empty index
-#         known_chars_prev = 0
-#         known_chars = known_chars_st.get_sum(next_empty_index, next_empty_index + d)
-
-#         while known_chars > known_chars_prev:
-#             diff = known_chars_st.get_sum(
-#                 next_empty_index + d + known_chars_prev,
-#                 next_empty_index + d + known_chars
-#             )
-#             known_chars_prev = known_chars
-#             known_chars += diff
-        
-#         print(next_empty_index,d, known_chars, len(text))
-#         print(text[next_empty_index:])
-#         text[next_empty_index-1+d+known_chars] = text[i]
-#         known_chars_st.update(next_empty_index-1+d+known_chars, 1)
-
-#         # update the next empty index
-#         while next_empty_index < len(text) and text[next_empty_index] is not None:
-#             next_empty_index += 1
-
-#         i += 1
-
-#     return ''.join(text[ALPHABET_SIZE:])
-
-
 def dc_decode(dc):
     text_length = dc[0]
     text = list(_get_alphabet()) + [None] * text_length
-    next_empty_index = ALPHABET_SIZE
+    known_chars_st = SegmentTree([1] * ALPHABET_SIZE + [0] * text_length)
 
     i = 0
     for d in dc[1:]:
-        # restore consecutive chars
-        while i < len(text) - 1 and text[i+1] == None:
+        # Restore consecutive chars
+        while i < len(text) - 1 and text[i+1] is None:
             text[i+1] = text[i]
             i += 1
 
@@ -107,23 +63,29 @@ def dc_decode(dc):
             i += 1
             continue
         
-        # find the dth empty index
-        dth_empty_index = next_empty_index
-        while True:
-            if text[dth_empty_index] == None:
-                d -= 1
-            
-            if d == 0:
-                break
-
-            dth_empty_index += 1
+        # Find the dth empty index.
+        # Go to the i + d index first, then
+        # calculate how many indices in between aren't empty and
+        # find the known_chars_num'th empty index.
+        # This is an optimization.
+        known_chars_num = known_chars_st.get_sum(i + 1, i + d) + 1
+        dth_empty_index = _find_kth_empty_index(text, i + d, known_chars_num)
 
         text[dth_empty_index] = text[i]
-        
-        # update the next empty index
-        while next_empty_index < len(text) and text[next_empty_index] != None:
-            next_empty_index += 1
+        known_chars_st.update(dth_empty_index, 1)
 
         i += 1
 
     return ''.join(text[ALPHABET_SIZE:])
+
+
+def _find_kth_empty_index(text, start_post, k):
+    i = 0
+    while True:
+        if text[start_post + i] is None:
+            k -= 1
+        if k == 0:
+            break
+        i += 1
+    return start_post + i
+
